4ad06ec36c992cd62564bf5fe7efbde596e537f8
DEVOPS2 <DEVOPS2@CAMPUS.LOCAL>
2022-06-21 15:25:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const session = require('express-session');
+const { request } = require('http');
+const app = express();
+const ejs = require('ejs');
+const port = 3000;
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({ 
+    secret: 'keyboard cat',
+    resave: false,
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true
+}))
+app.set('view engine','ejs');
+
+
+app.listen(port,function(){
+    console.log(`Serveur Express lancé sur le port : ${port}`)
+})
+
+
+app.get('/', function(request, response) {
+
+    const articles=[
+        {
+            titre: "Débuter avec Node.js",
+            categorie: "Front-end"
+        },
+        {
+            titre: "Débuter avec PHP",
+            categorie: "Backend"
+        },
+        {
+            titre: "Débuter avec Nyolo",
+            categorie: "Front-end"
+        },
+        {
+            titre: "Débuter avec Npouet",
+            categorie: "Front-end"
+        },
+    ]
+
+    const data = {articles};
+    response.render('home',data);
+});
+
+app.get('/:nom', function(request, response) {
+    const data = {
+        nom: request.params.nom
+    };
+    response.render('home',data);
+});
+
+
+app.use(function(request,response){
+    response.status(404).send('Page inexistante')
+});
+
